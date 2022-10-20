1215e4327a3d83614b91f08bcc651e34bccf8612
Rodrigo <99374549+rocabrera94@users.noreply.github.com>
2022-10-20 12:59:56 -0300

    Update main.js

--- index.html/main.js
+++ index.html/main.js
@@ -114,7 +114,7 @@ const timer = () =>{
     
     setInterval(()=>{
       if (num<= marcas.length-1){
-        imagen.src =`/index.html/img/anuncios/${marcas[num]}.jpg`;
+        imagen.src =`/img/anuncios/${marcas[num]}.jpg`;
         num++;
         console.log(imagen.src)
       } if (num>marcas.length-1) num=0;
